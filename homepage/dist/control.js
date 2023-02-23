4bf8ce49bb913d18a9b5aff4a55ea327bd1672c5
stmauda <120678111+StMauda@users.noreply.github.com>
2023-02-23 20:02:52 +0200

    logo upload

--- homepage/dist/control.js
+++ homepage/dist/control.js
@@ -25,6 +25,11 @@ roommateDivs.forEach(function (roommateDiv) {
         handleShowRoommate(roommateDiv.innerHTML);
     });
 });
+apartmentDivs.forEach(function (apartmentDiv) {
+    apartmentDiv.addEventListener("click", function () {
+        handleShowAapartment(apartmentDiv.innerHTML);
+    });
+});
 function handleShowRoommate(nameUser) {
     try {
         for (var i = 0; i < userProfils.length; i++) {
@@ -58,27 +63,22 @@ function hendelBtnBackToPage() {
         console.error(error);
     }
 }
-function handleShowAapartment(ev) {
+function handleShowAapartment(apartmenNames) {
     try {
-        // console.log(ev)
-        // const html = apartmentProfils
-        // .map((profile) => {
-        // return `
-        // <div class="boxMain__container__boxes">
-        // <img class="boxMain__container__boxes__img"
-        //     src= ${apartments.apartmentImages[0].urlPicture} alt="roommate Img">
-        // <h4>${apartments.city}</h4>
-        // <p>We have a-${apartments.rooms} rooms</p>
-        // <p>We live in ${apartments.flor} floor</p>
-        // <p>The price is ${apartments.price} NIS</p>
-        // </div>
-        // `;
-        // })
-        // .join(" ");
-        // // console.log(`htmlApartments ${html}`);
-        // const element = document.querySelector(".apartments");
-        // if (!element) throw new Error("Couldnt find element in the DOM");
-        // element.innerHTML = html;
+        console.log("apartmenNames");
+        for (var i = 0; i < apartmentProfils.length; i++) {
+            if (apartmenNames.indexOf(apartmentProfils[i].apartmentName) != -1) {
+                console.log(apartmentProfils[i].apartmentName);
+                var html = "<div class=\"ditail__box\">\n                    <botton class=\"profilBtn\" onclick=\"hendelBtnBackToPage()\">X</botton>    \n                    <img class=\"ditail__box__img\"\n                    src= " + apartmentProfils[i].apartmentImages[0] + " alt=\"roommate Img\">\n                    <h4>" + apartmentProfils[i].city + "</h4>\n                    <p>The address  - " + apartmentProfils[i].address + "</p>\n                    <p>The apartment city - " + apartmentProfils[i].city + " city</p>\n                    <p>The apartment area -  " + apartmentProfils[i].area + " area</p>\n                    <p>Animal - " + apartmentProfils[i].animal + "</p>\n                    <p>Smoke - " + apartmentProfils[i].smoke + " </p>\n                    <p>We live in " + apartmentProfils[i].flor + " flor</p>\n                    <p>We have " + apartmentProfils[i].rooms + " rooms</p>\n                    <p>The number of parters - " + apartmentProfils[i].partersNo + " parters</p>\n                    <p>The price is - " + apartmentProfils[i].price + " NIS</p>\n                    </div>";
+                console.log("html " + html);
+                var element = document.getElementById("ditails");
+                if (!element)
+                    throw new Error("Couldnt find element in the DOM");
+                element.innerHTML = html;
+                element.style.display = "block";
+                document.getElementById("containerApartment").style.display = "none";
+            }
+        }
     }
     catch (error) {
         console.error(error);
--- homepage/model.ts
+++ homepage/model.ts
@@ -53,6 +53,7 @@ class ApartmentProfil {
   uid: string;
   
   constructor(
+    public apartmentName: string,
     public address: string,
     public city: string,
     public neighbour:string,
@@ -74,16 +75,16 @@ class ApartmentProfil {
 
 const apartmentProfils:ApartmentProfil[] = [];
 
-apartmentProfils.push(new ApartmentProfil('Herzel 5', 'Tel aviv', 'North', 'Center', 'Yes', 'No', '4', '5', '4', '2000',  
+apartmentProfils.push(new ApartmentProfil('one', 'Herzel 5', 'Tel aviv', 'North', 'Center', 'Yes', 'No', '4', '5', '4', '2000',  
 [{id: uid(), urlPicture: 'https://i.pinimg.com/474x/f0/c7/82/f0c78244f383cf4ea53640a606ed7310.jpg', imageName: "out Apartment"},
 {id: uid(), urlPicture: 'https://i.pinimg.com/474x/f3/cf/a7/f3cfa7a0652500cbc58f1d41e9165cab.jpg', imageName: "Saloon"},
 {id: uid(), urlPicture: 'https://i.pinimg.com/474x/26/55/3a/26553ac29d6df41ea98ebfeca53390ab.jpg', imageName: "Badroom"},
 {id: uid(), urlPicture: 'https://i.pinimg.com/474x/e8/16/93/e8169353a711058d901c8994469ec443.jpg', imageName: "Livig Room"}]))
-apartmentProfils.push(new ApartmentProfil('Negev', 'Haifa', 'Bat galim' ,'North', 'No', 'Yes', '3', '4', '3', '3000', 
+apartmentProfils.push(new ApartmentProfil('two', 'Negev', 'Haifa', 'Bat galim' ,'North', 'No', 'Yes', '3', '4', '3', '3000', 
 [{id: uid(), urlPicture: 'https://e7.pngegg.com/pngimages/48/624/png-clipart-house-housing-cartoon-house-cartoon-character-houses-thumbnail.png', imageName: "out Apartment"},
 {id: uid(), urlPicture: 'https://i.pinimg.com/564x/26/55/3a/26553ac29d6df41ea98ebfeca53390ab.jpg', imageName: "Badroom"},
 {id: uid(), urlPicture: 'https://i.pinimg.com/474x/e3/89/7d/e3897df913172bde95d119589661425f.jpg', imageName: "Living Room"}]))
-apartmentProfils.push(new ApartmentProfil('Herzel 30', 'Holon', 'All', 'Merkaz', 'Yes', 'No', '4', '6', '5', '1500', 
+apartmentProfils.push(new ApartmentProfil('three', 'Herzel 30', 'Holon', 'All', 'Merkaz', 'Yes', 'No', '4', '6', '5', '1500', 
 [{id: uid(), urlPicture: 'https://e7.pngegg.com/pngimages/462/450/png-clipart-minecraft-house-building-house-building-interior-design-services-modern-house-furniture-building-thumbnail.png', imageName: "out Apartment"},
 {id: uid(), urlPicture: 'https://icon2.cleanpng.com/20180219/guq/kisspng-loft-window-office-room-stock-photography-building-floor-windows-high-clear-buckle-material-5a8aa45ccc55c5.914750281519035484837.jpg', imageName: "Living room"},
 {id: uid(), urlPicture: 'https://icon2.cleanpng.com/20180304/oue/kisspng-kitchenware-animation-cartoon-pressure-cooker-kitchen-furniture-show-5a9bf02969c104.3227696815201690014332.jpg', imageName: "kitchen"}]))
