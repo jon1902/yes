9bd6a2a68bb4c4c33287ef9264d6bbe1cb9271e1
jon <77334823+jon1902@users.noreply.github.com>
2023-03-12 00:17:32 +0200

    final

--- registration/control.ts
+++ registration/control.ts
@@ -16,13 +16,14 @@ if (regSubmitButton) {
       name: _name.value,
       tel: _tel.value,
       eMail: _email.value,
+
     });
     const registraionNewUsers:any[] = []
 
     if(registraionNewUsers){
     registraionNewUsers.push(_newUser);
     
-    localStorage.setItem("registraionNewUsers", JSON.stringify(users));
+    localStorage.setItem("registraionNewUsers", JSON.stringify(registraionNewUsers));
     console.log(registraionNewUsers)
     }
   });
@@ -32,3 +33,47 @@ if (regSubmitButton) {
 //   });
 }
 
+
+// if (submitButton) {
+//     submitButton.addEventListener("click", (e: Event) => {
+//       e.preventDefault();
+
+//     let newUser:User = new User(nameInput.value, "123");
+
+//     newUser.setDetails({
+//         name: nameInput.value,
+//         address: adressInput.value,
+//         city: cityInput.value,
+//         area: areaInput.value,
+//         animal: animalInput.value,
+//         smoke: smokeInput.value,
+//         floor: floorInput.value,
+//         rooms: roomsInput.value,
+//         partersNo: partersNoInput.value,
+//         price: priceInput.value,
+//         imgSrc: imgSrcInput.value,
+//     })
+
+//     users.push(newUser)
+       
+//     UserProfilList.push(newUser);
+//       localStorage.setItem("UserProfilList", JSON.stringify(UserProfilList));
+//       nameInput.value = "";
+//       adressInput.value = "";
+//       cityInput.value = "";
+//       areaInput.value = "";
+//       animalInput.value = "";
+//       smokeInput.value = "";
+//       floorInput.value = "";
+//       roomsInput.value = "";
+//       partersNoInput.value = "";
+//       priceInput.value = "";
+//       imgSrcInput.value = "";
+
+
+//     });
+
+//     submitButton.addEventListener("click", function() {
+//         window.location.assign('../homepage/homepage.html');
+//     });
+//   }
--- registration/dist/control.js
+++ registration/dist/control.js
@@ -16,7 +16,7 @@ if (regSubmitButton) {
         var registraionNewUsers = [];
         if (registraionNewUsers) {
             registraionNewUsers.push(_newUser);
-            localStorage.setItem("registraionNewUsers", JSON.stringify(users));
+            localStorage.setItem("registraionNewUsers", JSON.stringify(registraionNewUsers));
             console.log(registraionNewUsers);
         }
     });
@@ -24,3 +24,39 @@ if (regSubmitButton) {
     //     window.location.assign("../homepage/homepage.html");
     //   });
 }
+// if (submitButton) {
+//     submitButton.addEventListener("click", (e: Event) => {
+//       e.preventDefault();
+//     let newUser:User = new User(nameInput.value, "123");
+//     newUser.setDetails({
+//         name: nameInput.value,
+//         address: adressInput.value,
+//         city: cityInput.value,
+//         area: areaInput.value,
+//         animal: animalInput.value,
+//         smoke: smokeInput.value,
+//         floor: floorInput.value,
+//         rooms: roomsInput.value,
+//         partersNo: partersNoInput.value,
+//         price: priceInput.value,
+//         imgSrc: imgSrcInput.value,
+//     })
+//     users.push(newUser)
+//     UserProfilList.push(newUser);
+//       localStorage.setItem("UserProfilList", JSON.stringify(UserProfilList));
+//       nameInput.value = "";
+//       adressInput.value = "";
+//       cityInput.value = "";
+//       areaInput.value = "";
+//       animalInput.value = "";
+//       smokeInput.value = "";
+//       floorInput.value = "";
+//       roomsInput.value = "";
+//       partersNoInput.value = "";
+//       priceInput.value = "";
+//       imgSrcInput.value = "";
+//     });
+//     submitButton.addEventListener("click", function() {
+//         window.location.assign('../homepage/homepage.html');
+//     });
+//   }
