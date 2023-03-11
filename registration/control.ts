676ab6db8bc61b37c8ae39fa4f7b30cf077b829c
Shalom Copperman <shalomcop@gmail.com>
2023-03-11 22:58:42 +0200

    registration done

--- registration/control.ts
+++ registration/control.ts
@@ -1,22 +1,29 @@
-if(document.querySelector(".registrationBtn")){
-    const registrationBtn = document.querySelector(".registrationBtn") as HTMLButtonElement
-    registrationBtn.addEventListener("click",() =>{
-        try {
-            location.href = ('/homepage/homepage.html')
-        } catch (error) {
-            console.error(error)        
-        }
-    })
+
+const regSubmitButton = document.querySelector("#myButton");
+
+if (regSubmitButton) {
+  regSubmitButton.addEventListener("click", (e: Event) => {
+    e.preventDefault();
+    const _name = document.querySelector("#name") as HTMLInputElement;
+    const _email = document.querySelector("#email") as HTMLInputElement;
+    const _tel = document.querySelector("#tel") as HTMLInputElement;
+    const _username = document.querySelector("#username") as HTMLInputElement;
+    const _password = document.querySelector("#password") as HTMLInputElement;
+    
+    const _newUser: User = new User(_username.value, _password.value);
+
+    _newUser.setDetails({
+      name: _name.value,
+      tel: _tel.value,
+      eMail: _email.value,
+    });
+
+    users.push(_newUser);
+
+    localStorage.setItem("regNewUsers", JSON.stringify(users));
+  });
+
+  regSubmitButton.addEventListener("click", function () {
+    window.location.assign("../homepage/homepage.html");
+  });
 }
-
-const _name = document.querySelector("#name") as HTMLInputElement;
-const _email = document.querySelector("#email") as HTMLInputElement;
-const _tel = document.querySelector("#tel") as HTMLInputElement;
-const _username = document.querySelector("#username") as HTMLInputElement;
-const _password = document.querySelector("#password") as HTMLInputElement;
-
-const _newUser:User = new User(_username.value,_password.value);
-_newUser.setDetails({name:_name.value,tel:_tel.value,eMail:_email.value})
-
-localStorage.setItem('newUser', JSON.stringify(_newUser));
-  
--- registration/dist/control.js
+++ registration/dist/control.js
@@ -1,19 +1,22 @@
-if (document.querySelector(".registrationBtn")) {
-    var registrationBtn = document.querySelector(".registrationBtn");
-    registrationBtn.addEventListener("click", function () {
-        try {
-            location.href = ('/homepage/homepage.html');
-        }
-        catch (error) {
-            console.error(error);
-        }
+var regSubmitButton = document.querySelector("#myButton");
+if (regSubmitButton) {
+    regSubmitButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        var _name = document.querySelector("#name");
+        var _email = document.querySelector("#email");
+        var _tel = document.querySelector("#tel");
+        var _username = document.querySelector("#username");
+        var _password = document.querySelector("#password");
+        var _newUser = new User(_username.value, _password.value);
+        _newUser.setDetails({
+            name: _name.value,
+            tel: _tel.value,
+            eMail: _email.value
+        });
+        users.push(_newUser);
+        localStorage.setItem("regNewUsers", JSON.stringify(users));
+    });
+    regSubmitButton.addEventListener("click", function () {
+        window.location.assign("../homepage/homepage.html");
     });
 }
-var _name = document.querySelector("#name");
-var _email = document.querySelector("#email");
-var _tel = document.querySelector("#tel");
-var _username = document.querySelector("#username");
-var _password = document.querySelector("#password");
-var _newUser = new User(_username.value, _password.value);
-_newUser.setDetails({ name: _name.value, tel: _tel.value, eMail: _email.value });
-localStorage.setItem('newUser', JSON.stringify(_newUser));
